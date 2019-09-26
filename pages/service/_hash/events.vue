5b95a214715af10eea2fad216da2bd3ab5709d3b
Anthony ESTEBE <anthony@mesg.com>
2019-09-26 18:43:33 +0700

    add task and events

--- pages/service/_hash/events.vue
+++ pages/service/_hash/events.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>event {{ service }}</div>
+  <div>
+    <h2 class="headline mb-4">Events</h2>
+    <v-expansion-panels accordion>
+      <v-expansion-panel v-for="event in events" :key="event.key">
+        <v-expansion-panel-header>{{
+          event.name || event.key
+        }}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <p>{{ event.description }}</p>
+          <h3 class="subtitle-1">Data</h3>
+          <ParameterTable class="mb-6" :params="event.data || []" />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script>
+import ParameterTable from '~/components/ParameterTable'
 export default {
+  components: {
+    ParameterTable
+  },
   props: {
     service: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    events() {
+      return this.service.events
     }
   }
 }
--- pages/service/_hash/tasks.vue
+++ pages/service/_hash/tasks.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-expansion-panels accordion>
-    <v-expansion-panel v-for="task in tasks" :key="task.key">
-      <v-expansion-panel-header>{{
-        task.name || task.key
-      }}</v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <p>{{ task.description }}</p>
-        <h3 class="mb-4">Inputs</h3>
-        <ParameterTable class="mb-6" :params="task.inputs || []" />
-        <h3 class="mb-4">Outputs</h3>
-        <ParameterTable :params="task.outputs || []" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-expansion-panels>
+  <div>
+    <h2 class="headline mb-4">Tasks</h2>
+    <v-expansion-panels accordion>
+      <v-expansion-panel v-for="task in tasks" :key="task.key">
+        <v-expansion-panel-header>{{
+          task.name || task.key
+        }}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <p>{{ task.description }}</p>
+          <h3 class="subtitle-1">Inputs</h3>
+          <ParameterTable class="mb-6" :params="task.inputs || []" />
+          <h3 class="subtitle-1">Outputs</h3>
+          <ParameterTable :params="task.outputs || []" />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script>
