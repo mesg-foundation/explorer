46c2ac9c80810071e46cc3d7437543756f61bf62
Anthony ESTEBE <anthony@mesg.com>
2019-09-26 18:40:01 +0700

    add list of tasks

--- pages/service/_hash/tasks.vue
+++ pages/service/_hash/tasks.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>task {{ service }}</div>
+  <v-expansion-panels accordion>
+    <v-expansion-panel v-for="task in tasks" :key="task.key">
+      <v-expansion-panel-header>{{
+        task.name || task.key
+      }}</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <p>{{ task.description }}</p>
+        <h3 class="mb-4">Inputs</h3>
+        <ParameterTable class="mb-6" :params="task.inputs || []" />
+        <h3 class="mb-4">Outputs</h3>
+        <ParameterTable :params="task.outputs || []" />
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
+import ParameterTable from '~/components/ParameterTable'
 export default {
+  components: {
+    ParameterTable
+  },
   props: {
     service: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    tasks() {
+      return this.service.tasks
     }
   }
 }
