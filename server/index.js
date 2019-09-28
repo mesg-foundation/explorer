51fbe9774e67fc266c7dee6343f0cf388505189a
Anthony ESTEBE <anthony@mesg.com>
2019-09-28 15:18:44 +0700

    add cors protection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const app = require('express')()
+const cors = require('cors')
 const mesg = require('mesg-js/lib/api').default('localhost:50052')
 const { decode } = require('mesg-js/lib/util/base58')
 const getReadme = require('./readme')
+
+app.use(cors())
 
 app.get('/services', async (req, res) => res.json(await mesg.service.list({})))
 app.get('/services/:hash', async (req, res) => res.json(await mesg.service.get({ hash: decode(req.params.hash) })))
