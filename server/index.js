5b91557b9d519c518f2bf434e1bd18c259c5691a
Anthony ESTEBE <anthony@mesg.com>
2019-09-28 14:46:53 +0700

    add readme api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const app = require('express')()
 const mesg = require('mesg-js/lib/api').default('localhost:50052')
 const { decode } = require('mesg-js/lib/util/base58')
+const getReadme = require('./readme')
 
 app.get('/services', async (req, res) => res.json(await mesg.service.list({})))
 app.get('/services/:hash', async (req, res) => res.json(await mesg.service.get({ hash: decode(req.params.hash) })))
+app.get('/readme/:hash', async (req, res) => res.send(await getReadme(req.params.hash)))
 
 app.listen(3001, () => console.log('started'))
---
+++ server/readme.js
@@ -0,0 +1,52 @@
+const LRU = require('lru-cache')
+const ipfsClient = require('ipfs-http-client')
+const tar = require('tar')
+const fs = require('fs')
+const { join } = require('path')
+const { tmpdir } = require('os')
+const MarkdownIt = require('markdown-it')
+
+
+const md = new MarkdownIt()
+const IPFS = ipfsClient('ipfs.app.mesg.com', '5001', { protocol: 'http' })
+const readme = new LRU({})
+
+const REGEXP = /^readme.md$/i
+
+const download = async (hash) => {
+  const tarball = await IPFS.get(hash)
+  if (tarball.length !== 1) throw new Error('invalid fetch')
+  return tarball[0].content
+}
+
+const extract = async (content, hash) => {
+  const tarFile = join(tmpdir(), `${hash}.tar`)
+  fs.writeFileSync(tarFile, content)
+  const target = join(tmpdir(), hash)
+  if (!fs.existsSync(target)) {
+    fs.mkdirSync(target)
+  }
+  await fs.createReadStream(tarFile).pipe(tar.extract({
+    cwd: target,
+    filter: (path, stat) => path.match(REGEXP)
+  }))
+  fs.unlinkSync(tarFile)
+  return target
+}
+
+const parseReadme = async (path) => {
+  const readmeFile = fs.readdirSync(path).find(x => x.match(REGEXP))
+  const html = md.render(fs.readFileSync(join(path, readmeFile)).toString())
+  return html
+}
+
+module.exports = async (hash) => {
+  if (readme.has(hash)) {
+    return readme.get(hash)
+  }
+  const content = await download(hash)
+  const path = await extract(content, hash)
+  const html = await parseReadme(path)
+  readme.set(hash, html)
+  return html
+}
