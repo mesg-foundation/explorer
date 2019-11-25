a6b69735b9e3dba66a017d9ad2fbf154b32c2f12
Anthony ESTEBE <anthony@mesg.com>
2019-11-25 13:59:20 +0700

    add execution detail

--- pages/executions/_hash.vue
+++ pages/executions/_hash.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="instance">
+  <div v-if="execution">
     <Header>
-      <h1 class="display-1">Instance {{ instance.hash }}</h1>
+      <h1 class="display-1">Execution {{ execution.hash }}</h1>
     </Header>
     <v-divider />
     <v-container>
@@ -16,39 +16,47 @@
                     <v-list-item-subtitle>Service name</v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item>
+                <v-list-item :to="`/instances/${instance.hash}`" nuxt>
+                  <v-list-item-content>
+                    <v-list-item-title>{{ instance.hash }}</v-list-item-title>
+                    <v-list-item-subtitle>Instance hash</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
                 <v-list-item>
                   <v-list-item-content>
                     <v-list-item-title>{{
-                      encode(instance.envHash)
+                      execution.tags.join(', ')
                     }}</v-list-item-title>
-                    <v-list-item-subtitle
-                      >Environment Hash</v-list-item-subtitle
-                    >
+                    <v-list-item-subtitle>Tags</v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item>
-              </v-list>
-            </v-card>
-          </v-col>
-          <v-col sm="4">
-            <v-card>
-              <v-card-title>Runners</v-card-title>
-              <v-list>
-                <v-list-item
-                  v-for="runner in runners"
-                  :key="runner.hash"
-                  :to="`/runners/${runner.hash}`"
-                  nuxt
-                >
+                <v-list-item>
                   <v-list-item-content>
-                    <v-list-item-title>{{ runner.address }}</v-list-item-title>
-                    <v-list-item-subtitle
-                      >Runner's address</v-list-item-subtitle
-                    >
+                    <v-list-item-title>{{
+                      execution.taskKey
+                    }}</v-list-item-title>
+                    <v-list-item-subtitle>Task</v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item>
               </v-list>
+              <v-card-text>
+                <code style="width: 100%;">
+                  {{ JSON.stringify(execution.inputs, null, 2) }}
+                </code>
+              </v-card-text>
+            </v-card>
+            <v-card class="mt-3">
+              <v-card-text>
+                <span v-if="execution.error" class="error--text">{{
+                  execution.error
+                }}</span>
+                <code v-else style="width: 100%;">
+                  {{ JSON.stringify(execution.outputs, null, 2) }}
+                </code>
+              </v-card-text>
             </v-card>
           </v-col>
+          <v-col sm="4"> </v-col>
         </v-row>
       </v-layout>
     </v-container>
@@ -63,28 +71,27 @@ export default {
   components: { Header },
   computed: {
     ...mapGetters({
-      instances: 'instances/list',
+      executions: 'executions/list',
       services: 'services/list',
-      _runners: 'runners/list'
+      instances: 'instances/list'
     }),
-    instance() {
-      return this.instances[this.$route.params.hash]
+    execution() {
+      return this.executions[this.$route.params.hash]
     },
-    runners() {
-      return Object.keys(this._runners)
-        .map((x) => this._runners[x])
-        .filter((x) => encode(x.instanceHash) === this.$route.params.hash)
+    instance() {
+      return this.instances[encode(this.execution.instanceHash)]
     },
     service() {
       return this.services[encode(this.instance.serviceHash)]
     }
   },
   fetch: async ({ store, params }) => {
-    const instance = await store.dispatch('instances/get', params.hash)
-    return Promise.all([
-      store.dispatch('services/get', encode(instance.serviceHash)),
-      store.dispatch('runners/list')
-    ])
+    const execution = await store.dispatch('executions/get', params.hash)
+    const instance = await store.dispatch(
+      'instances/get',
+      encode(execution.instanceHash)
+    )
+    await store.dispatch('services/get', encode(instance.serviceHash))
   },
   methods: { encode }
 }
