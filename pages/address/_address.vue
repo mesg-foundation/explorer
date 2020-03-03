18b4b77d1214a227adc69f739e5912ed80bee798
antho1404 <anthony@mesg.com>
2020-03-03 13:15:43 +0700

    remove send function

--- pages/address/_address.vue
+++ pages/address/_address.vue
@@ -6,48 +6,6 @@
       <List :items="coins" />
       <v-divider />
       <List :items="meta" />
-      <v-divider />
-      <v-card-actions>
-        <v-spacer />
-        <v-dialog>
-          <template v-slot:activator="{ on }">
-            <v-btn v-on="on" color="primary" dark>Send</v-btn>
-          </template>
-          <v-card>
-            <v-card-title>Transfer</v-card-title>
-            <v-divider />
-            <v-card-text>
-              <v-text-field
-                v-model="transfer.to"
-                label="Receipient"
-                required
-              ></v-text-field>
-              <v-layout>
-                <v-flex xs8>
-                  <v-text-field
-                    v-model="transfer.amount"
-                    label="Amount"
-                    required
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs4>
-                  <v-select
-                    v-model="transfer.denom"
-                    :items="account.value.coins"
-                    item-text="denom"
-                    item-value="denom"
-                  />
-                </v-flex>
-              </v-layout>
-            </v-card-text>
-            <v-divider />
-            <v-card-actions>
-              <v-spacer />
-              <v-btn @click="sendTransfer" color="primary">Send</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-card-actions>
     </v-card>
   </v-container>
 </template>
@@ -57,15 +15,6 @@ import List from '~/components/List'
 export default {
   components: {
     List
-  },
-  data() {
-    return {
-      transfer: {
-        to: 'mesgtest1yztk7h0la9e2wdhed3h37sd50szq8hsvrvgump',
-        denom: 'atto',
-        amount: '10'
-      }
-    }
   },
   computed: {
     address() {
@@ -92,9 +41,6 @@ export default {
         address: route.params.address
       })
     }
-  },
-  methods: {
-    sendTransfer() {}
   }
 }
 </script>
