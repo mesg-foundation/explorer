7347464c5ba220f7f47265e842013f42369bf892
antho1404 <anthony@mesg.com>
2020-03-03 11:37:01 +0700

    update chain id

--- api/tendermint/cosmos.js
+++ api/tendermint/cosmos.js
@@ -1,7 +1,7 @@
 import cosmosjs from '@cosmostation/cosmosjs'
 
 export const COSMOS_LCD = `http://${process.env.ENGINE_HOST}:1317`
-export const CHAIN_ID = 'mesg-testnet-01'
+export const CHAIN_ID = 'mesg-testnet-02'
 export const HD_PATH = "m/44'/470'/0'/0/0"
 export const BECH32_PREFIX = 'mesgtest'
 
