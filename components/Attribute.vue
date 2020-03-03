e3b383a4c708324ba8c488d27e93016676e5df1d
antho1404 <anthony@mesg.com>
2020-03-03 14:54:53 +0700

    link address attributes to the address url

--- components/Attribute.vue
+++ components/Attribute.vue
@@ -1,8 +1,11 @@
 <template>
-  <v-chip small class="mr-2" label outlined>{{ key }} = {{ value }}</v-chip>
+  <v-chip :to="to" small class="mr-2" label outlined nuxt
+    >{{ key }} = {{ value }}</v-chip
+  >
 </template>
 
 <script>
+import { BECH32_PREFIX } from '~/api/tendermint/cosmos'
 export default {
   props: {
     attribute: {
@@ -16,6 +19,13 @@ export default {
     },
     value() {
       return this.decode(this.attribute.value)
+    },
+    to() {
+      const addressRegexp = new RegExp(`^${BECH32_PREFIX}[a-z0-9]*$`)
+      if (this.value.match(addressRegexp)) {
+        return `/address/${this.value}`
+      }
+      return null
     }
   },
   methods: {
